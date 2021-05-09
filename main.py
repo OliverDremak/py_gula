64df90fbde87da1910f0253d04a447248354fe62
OliverDremak <72332685+OliverDremak@users.noreply.github.com>
2021-05-09 10:49:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+T=int(input("Kérem adja meg az alapját:"))
+m=int(input("Kérem adja meg a magasságát:"))
+P=int(input("Kérem adja meg a palást nagyságát:"))
+
+felszin=T+P 
+terfogat=T*m/3
+
+print(print(f'Térfogata:{terfogat}'))
+print(print(f'Felszine:{felszin}'))
